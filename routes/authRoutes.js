a63b0d46bad4ba60b987b99b0ad401eab39c6f32
Daan Sneep <daansneep@hotmail.nl>
2021-01-25 15:10:34 +0100

    Bugfix: Spelling error in route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -15,7 +15,7 @@ router.post('/login', authController.login)
 
 // POST /login/admin
 // Login an admin who can change the content on the site
-router.post('login/admin', authController.login)
+router.post('/login/admin', authController.login)
 
 // POST /register/admin
 // Register another admin who can change the content on the site
